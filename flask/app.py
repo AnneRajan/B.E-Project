f65df766d6adbd7016c45b9c63be76ea302233cb
Anne Rajan <50075447+AnneRajan@users.noreply.github.com>
2020-04-04 01:16:28 +0530

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -15,16 +15,42 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    y=model.predict( np.array( [final_features,] )  )
+    result = np.where(y == np.amax(y))
+    
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+   
+   
 
-    output = round(prediction[0], 16)
+   
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+#Here i am printing this on the terminal but u have to put it like this on the website
+
+    if result[0]==[0]:
+        return render_template('index.html', prediction_text='Business Intelligence')
+        print('Business Intelligence Analyst')
+    elif result[0]==[1]:
+        return render_template('index.html', prediction_text='Database Administrator')
+        print('Database Administrator')
+    elif result[0]==[2]:
+        return render_template('index.html', prediction_text='Project Manager')
+        print('Project Manager')
+    elif result[0]==[3]:
+        return render_template('index.html', prediction_text='Security Administrator')
+        print('Security Administrator')
+    elif result[0]==[4]:
+        return render_template('index.html', prediction_text='Software Developer')
+        print('Software Developer')
+    else:
+        return render_template('index.html', prediction_text='Technical Support')
+        print('Technical Support')
+   
+
+   # return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
 if __name__ == "__main__":
