6b68548eb443fe66b56ee90a1c74f58d7d31c4e0
vdeo269 <varaddeo269@gmail.com>
2020-04-16 09:10:56 +0530

    flask

--- flask/app.py
+++ flask/app.py
@@ -4,29 +4,146 @@ from flask import Flask, request, jsonify, render_template
 import tensorflow as tf
 import keras
 from keras.models import load_model
+from tensorflow.python.keras.backend import set_session
+from tensorflow.python.keras.models import load_model
 
 app = Flask(__name__)
 
+
 model = load_model('Job_Role_model.h5')
+graph = tf.get_default_graph()
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    y=model.predict( np.array( [final_features,] )  )
-    result = np.where(y == np.amax(y))
-    
-    '''
-    For rendering results on HTML GUI
-    '''
-   
-   
+@app.route('/prediction',methods=['POST'])
+def prediction():
 
-   
+    os          = request.form["os"]
+    aoa         = request.form["aoa"]
+    pc          = request.form["pc"]
+    se          = request.form["se"]
+    cn          = request.form["cn"]
+    ma          = request.form["ma"]
+    cs          = request.form["cs"]
+    hac         = request.form["hac"]
+    interest    = request.form["interest"]
+    cert        = request.form["cert"]
+    personality = request.form["personality"]
+    mantech     = request.form["mantech"]
+    leadership  = request.form["leadership"]
+    team        = request.form["team"]
+    selfab      = request.form["selfab"]
+
+    myu = [77.00318789848731, 76.99831228903614, 77.07569696212026, 77.11301412676585, 76.9541817727216, 77.0150018752344, 77.060320040005, 5.002687835979497]
+    sig = [10.071578660726848, 10.098653693844197, 10.137528173238477, 10.088164425588161, 10.018397202418788, 10.18533143324003, 10.095941558583263, 2.582645138598079]
+    arr = [os,aoa,pc,se,cn,ma,cs,hac]
+
+    for i in range(8):
+        arr[i] = float(arr[i])
+        arr[i] = (arr[i]- myu[i])/sig[i]
+
+    inti     = [0,0,0,0,0,0,0,0,0,0,0,0,0]
+    certi    = [0,0,0,0,0,0,0]
+
+    if interest == "analyst":
+        inti[0] = 1
+    elif interest == "hadoop":
+        inti[1] = 1
+    elif interest == "cloud":
+        inti[2] = 1
+    elif interest == "data":
+        inti[3] = 1
+    elif interest == "hacking":
+        inti[4] = 1
+    elif interest == "management":
+        inti[5] = 1
+    elif interest == "networks":
+        inti[6] = 1
+    elif interest == "programming":
+        inti[7] = 1
+    elif interest == "security":
+        inti[8] = 1
+    elif interest == "software":
+        inti[9] = 1
+    elif interest == "system":
+        inti[10] = 1
+    elif interest == "testing":
+        inti[11] = 1
+    elif interest == "web":
+        inti[12] = 1
+
+    if cert == "app":
+        certi[0] = 1
+    elif cert == "full":
+        certi[1] = 1
+    elif cert == "hadoop":
+        certi[2] = 1
+    elif cert == "security":
+        certi[3] = 1
+    elif cert == "machine":
+        certi[4] = 1
+    elif cert == "python":
+        certi[5] = 1
+    elif cert == "shell":
+        certi[6] = 1
+
+    for i in certi:
+        arr.append(i)
+
+    for i in inti:
+        arr.append(i)
+
+    if leadership == "yesl":
+        arr.append(0)
+        arr.append(1)
+    else:
+        arr.append(1)
+        arr.append(0)
+
+    if team == "yest":
+        arr.append(0)
+        arr.append(1)
+    else:
+        arr.append(1)
+        arr.append(0)
+
+    if personality == "extrovert":
+        arr.append(1)
+        arr.append(0)
+    else:
+        arr.append(0)
+        arr.append(1)
+
+    if selfab == "nos":
+        arr.append(1)
+        arr.append(0)
+    else:
+        arr.append(0)
+        arr.append(1)
+
+    if mantech == "man":
+        arr.append(1)
+        arr.append(0)
+    else:
+        arr.append(0)
+        arr.append(1)
+
+
+    print ('arr ',arr)
+
+    with graph.as_default():
+        print("done in loop")
+        y      = model.predict(np.array( [arr,]))
+        result = np.where(y == np.amax(y))
+        print(y)
+        print(result)
+
+    print("done1")
+
+
 
 #Here i am printing this on the terminal but u have to put it like this on the website
 
@@ -48,7 +165,8 @@ def predict():
     else:
         return render_template('index.html', prediction_text='Technical Support')
         print('Technical Support')
-   
+
+    print("done2")
 
    # return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
