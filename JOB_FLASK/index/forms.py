283db90937af3b52d3c744f0d0a6987b3bb85446
Sayali <sayalideo137@gmail.com>
2020-05-10 20:23:28 +0530

    profile pic working in /account

--- JOB_FLASK/index/forms.py
+++ JOB_FLASK/index/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from wtforms.fields.html5 import EmailField
 from index.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     firstname        = StringField('First Name', validators=[DataRequired()]) # DataRequired means it cant be empty
@@ -12,15 +14,27 @@ class RegistrationForm(FlaskForm):
     password         = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit           = SubmitField('Sign Up')
-    def validate_firstname(self, firstname):
-        user = User.query.filter_by(firstname = firstname.data).first()
-        if user:
-            raise ValidationError('User Already Exists!')
-
+    
     def validate_email(self, email):
         user = User.query.filter_by(email = email.data).first()
         if user:
             raise ValidationError('Email Already Exists!')
+
+class UpdateForm(FlaskForm):
+    firstname = StringField('First Name', validators=[DataRequired()]) # DataRequired means it cant be empty
+    lastname  = StringField('Last Name', validators=[DataRequired()])
+    college   = StringField('College', validators=[DataRequired()])
+    email     = EmailField('Email', validators=[DataRequired(), Email()])
+    picture   = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    submit    = SubmitField('Sign Up')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user:
+                raise ValidationError('Email Already Exists!')
+
 
 class LoginForm(FlaskForm):
     email    = EmailField('Email', validators=[DataRequired(), Email()])
